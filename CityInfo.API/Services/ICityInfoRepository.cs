015cd3c4ed4de87b945a6914d92815c7715036b4
Gong-Code <gong.moonphruk@gmail.com>
2022-05-26 15:59:37 +0200

    Add Asynchronousand and implement data from Repository

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,30 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+
+        Task<City?> GetCityAsync(int cityId, bool includePointOfInterest);
+
+        Task<bool> CityExistsAsync(int cityId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId,
+            int pointOfInterestId);
+
+        Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
+
+        void DeletePointOfInterest(PointOfInterest pointOfInterest);
+
+        Task<bool> SaveChangesAsync();
+        
+        
+        /* The Purpose of Async Code
+         * Freeing up threads so they can be used for other task,
+         * which improves the scalability of your application */
+
+    }
+}
